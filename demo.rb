102f8bf03a2d3930330de9db7cb67c4904776d38
JJ Asghar <jjasghar@gmail.com>
2015-10-29 09:39:55 +0900

    i can't ever remember where floating_ip_pool goes

--- demo.rb
+++ demo.rb
@@ -5,9 +5,9 @@ with_machine_options({
                          :flavor_ref  => 3,
                          :image_ref => '741b48c5-3575-4e91-a3ee-0c1f2c38432b',
                          :nics => [{ :net_id => '8fc162c0-311e-4006-ba81-89afddb9db6c'}],
-                         :key_name => 'jdizzle'
+                         :key_name => 'jdizzle',
+                         :floating_ip_pool => 'ext-net'
                        },
-                       :floating_ip_pool => 'ext-net',
                        :ssh_username => 'ubuntu'
                      })
 
@@ -29,9 +29,9 @@ machine 'qa-webserver' do
                       :flavor_ref  => 3,
                       :nics => [{ :net_id => '8fc162c0-311e-4006-ba81-89afddb9db6c'}],
                       :key_name => 'jdizzle',
-                      :image_ref => '28070300-8b4a-4568-837a-dc48d96d5d50'
+                      :image_ref => '28070300-8b4a-4568-837a-dc48d96d5d50',
+                      :floating_ip_pool => 'ext-net'
                     },
-                    :floating_ip_pool => 'ext-net',
                     :ssh_username => 'centos'
                   })
   recipe 'demo-chef-provisioning::default'
